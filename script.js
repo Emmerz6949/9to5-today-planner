490c130e03cc9d1c43f31232ce6a83bfa6483b4a
emmerz6949 <emmerz6949@gmail.com>
2020-06-25 15:26:44 -0400

    added comments to the index.html file, the script.js file, and the style.css file

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+//all code inside this function will execute after the webpage has fully loaded
 $(document).ready(function() {
+    //uses Moment.js to get the day of the week, month, and numerical date of the day
     var today = moment().format('dddd, MMMM Do');
+    //uses Moment.js to get the current hour
     var currentHour = moment().format('ha');
+    //gets all elements from index.html that have the class hour
     var pageHours = $('.hour');
+    //gets all textarea elements from index.html
     var hourText = $('textarea');
 
+    //sets the text of the element (from index.html) with the id currentDay to the info stored in the today variable
     $('#currentDay').text(today);
 
+    /*a for loop that assigns the value of each textarea in the index.html file to the 
+    last corresponding value saved in local storage, and changes the background colors of 
+    the textarea elements (from the index.html file) based on the time of day*/
     for (i=0; i < pageHours.length; i++) {
         $(hourText[i]).val(localStorage.getItem(i + 'events'));
         
@@ -26,6 +35,9 @@ $(document).ready(function() {
         }
     }
 
+    //adds a click event listener to all elements that have the class saveBtn
+    /*when an element with the saveBtn class is clicked, the value of its sibling that has 
+    the class description will be saved in local storage*/
     $('.saveBtn').on('click', function() {
         event.preventDefault();
 
